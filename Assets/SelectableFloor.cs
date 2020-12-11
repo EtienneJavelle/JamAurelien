66ec0c523cfa19d4063d6f1876132c29d87c266a
etiennejavelle <etienne.javelle@lacatholille.fr>
2020-12-11 11:40:42 +0100

    Movements de base

---
+++ Assets/SelectableFloor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectableFloor : MonoBehaviour
+{
+    public bool selected = false;
+    public bool canMove = false;
+    public bool blocked = false;
+    public Material[] materials;
+    void Select()
+    {
+        this.transform.localScale = Vector3.one * 1.1f;
+    }
+    void DeSelect()
+    {
+        this.transform.localScale = Vector3.one;
+    }
+
+    private void Update()
+    {
+        if (selected) Select();
+        else DeSelect();
+        if (canMove) Walkable();
+        else NonWalkable();
+        if (blocked) Blocked();
+    }
+
+    private void Blocked()
+    {
+        this.GetComponent<Renderer>().material = materials[2];
+    }
+
+    void NonWalkable()
+    {
+        this.GetComponent<Renderer>().material = materials[0];
+    }
+
+    void Walkable()
+    {
+        this.GetComponent<Renderer>().material = materials[1];
+    }
+}
---
+++ Assets/SquadMember.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquadMember : MonoBehaviour
+{
+    public int mouveSpeed=1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
