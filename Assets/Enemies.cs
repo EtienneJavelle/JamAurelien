e3c9e56050282fc41dea926450df56df33a0726a
etiennejavelle <etienne.javelle@lacatholille.fr>
2020-12-11 16:32:22 +0100

    Revert "Movement ennemi"
    This reverts commit 32c4cb86df3a060205a9c1114917ee0a1f0894f0.

--- Assets/Enemies.cs
+++ Assets/Enemies.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class Enemies : MonoBehaviour
 {
@@ -32,44 +31,23 @@ public class Enemies : MonoBehaviour
     {
         if (enemyTurn)
         {
-            enemyturn = 0;
             foreach (Enemy enemy in enemies)
             {
-                MoveEnemy(1, enemy.transform.position, enemy);
-                enemyturn++;
+                Vector3 closest = Vector3.one * 1000;
+                foreach (SquadMember squadMember in squadMembers)
+                {
+                    Vector3 tmp = Tools.YZero(squadMember.transform.position-enemy.transform.position);
+                    if (tmp.x < closest.x && tmp.y < closest.z) closest = tmp;
+                }
+
             }
-            Invoke("EndMove", .4f);
-            enemyTurn = false;
-            GameObject u = GameObject.Find("Team");
-            u.GetComponent<SquadControl>().teamMemberTurn = 0;
         }
     }
 
 
-    void MoveEnemy(int dist, Vector3 dir, Enemy enemy)
+    void MoveEnemy(int dist, Vector3 dir)
     {
         dir.y = .9f;
-        dir.x += (int)UnityEngine.Random.Range(-1, 3);
-        dir.z += (int)UnityEngine.Random.Range(-1, 3);
-        dir.x = Mathf.Clamp(dir.x, 0, GameObject.Find("Map").GetComponent<MapCreator>().mapSize.x-1);
-        dir.z = Mathf.Clamp(dir.z, 0, GameObject.Find("Map").GetComponent<MapCreator>().mapSize.y-1);
-
-        foreach (SelectableFloor selectable in selectableFloors)
-        {
-            SelectableFloor under = Array.Find(selectableFloors, p => Tools.YZero(p.transform.position) == Tools.YZero(enemy.transform.position));
-            under.enemy = null;
-        }
         enemies[enemyturn].transform.DOMove(dir, .4f, false);
-    }
-    void EndMove()
-    {
-        foreach (var enemy in enemies)
-        {
-            foreach (SelectableFloor selectable in selectableFloors)
-            {
-                SelectableFloor under = Array.Find(selectableFloors, p => Tools.YZero(p.transform.position) == Tools.YZero(enemy.transform.position));
-                under.enemy = enemy;
-            }
-        }
     }
 }
