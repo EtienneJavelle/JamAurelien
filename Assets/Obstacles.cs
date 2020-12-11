aa9dfbf173f5f9d5d6fe6145b1cc0a53288e32c9
etiennejavelle <etienne.javelle@lacatholille.fr>
2020-12-11 12:17:14 +0100

    Basic Block

---
+++ Assets/Obstacles.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    public Transform[] obstacles;
+    [SerializeField]
+    SelectableFloor[] selectableFloors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject u = GameObject.Find("Team");
+        selectableFloors = u.GetComponent<SquadControl>().selectableFloors;
+        obstacles = this.gameObject.GetComponentsInChildren<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Transform obstacle in obstacles)
+        {
+            SelectableFloor under = Array.Find(selectableFloors, p => Tools.YZero(p.transform.position) == Tools.YZero(obstacle.position));
+            under.canMove = false;
+            under.blocked = true;
+        }
+    }
+}
--- Assets/SquadControl.cs
+++ Assets/SquadControl.cs
@@ -12,33 +12,26 @@ public class SquadControl : MonoBehaviour
     public int teamMemberTurn = 0;
 
     public float turnSpeed = .4f;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        selectableFloors = this.gameObject.GetComponentsInChildren<SelectableFloor>();
         NewTurn();
     }
 
     // Update is called once per frame
     void Update()
-    {
-
-        //if (Input.GetKeyDown(KeyCode.RightArrow)) MoveMember(1, new Vector3(1,0,0));
-        //if (Input.GetKeyDown(KeyCode.LeftArrow)) MoveMember(1, new Vector3(-1, 0, 0));
-        //if (Input.GetKeyDown(KeyCode.UpArrow)) MoveMember(1, new Vector3(0, 0, 1));
-        //if (Input.GetKeyDown(KeyCode.DownArrow)) MoveMember(1, new Vector3(0, 0, -1));
-
-        
-
-
-            
+    {            
         foreach (SelectableFloor selectableFloor in selectableFloors) selectableFloor.selected = false;
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit)) hit.transform.gameObject.GetComponent<SelectableFloor>().selected = true; ;
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1")&&Tools.Timer.Check(nextTurn))
         {
             if (hit.transform.gameObject.GetComponent<SelectableFloor>().canMove)
             {
+                nextTurn = Tools.Timer.New(turnSpeed);
                 MoveMember(1, hit.transform.position);
                 teamMemberTurn++;
                 if (teamMemberTurn >= squadMembers.Length) teamMemberTurn = 0;
@@ -57,7 +50,7 @@ public class SquadControl : MonoBehaviour
                 selectableFloor.transform.position.x >= squadMembers[teamMemberTurn].transform.position.x - squadMembers[teamMemberTurn].mouveSpeed &&
                 selectableFloor.transform.position.z <= squadMembers[teamMemberTurn].transform.position.z + squadMembers[teamMemberTurn].mouveSpeed &&
                 selectableFloor.transform.position.z >= squadMembers[teamMemberTurn].transform.position.z - squadMembers[teamMemberTurn].mouveSpeed &&
-                YZero(selectableFloor.transform.position) != YZero(squadMembers[teamMemberTurn].transform.position))
+                Tools.YZero(selectableFloor.transform.position) != Tools.YZero(squadMembers[teamMemberTurn].transform.position))
                 selectableFloor.canMove = true;
             else
             {
@@ -66,7 +59,7 @@ public class SquadControl : MonoBehaviour
         }
         foreach (SquadMember squadMember in squadMembers)
         {
-            SelectableFloor under = Array.Find(selectableFloors, p => YZero(p.transform.position) == YZero(squadMember.transform.position));
+            SelectableFloor under = Array.Find(selectableFloors, p => Tools.YZero(p.transform.position) == Tools.YZero(squadMember.transform.position));
             under.canMove = false;
             under.blocked = true;
         }
@@ -84,8 +77,5 @@ public class SquadControl : MonoBehaviour
         Invoke("NewTurn", turnSpeed+.1f);
     }
 
-    Vector3 YZero(Vector3 pos)
-    {
-        return new Vector3(pos.x, 0, pos.z);
-    }
+    float nextTurn;
 }
